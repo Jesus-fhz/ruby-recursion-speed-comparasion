b80fc25a91ff2e4eb1d53365bfeb4eca6dd6cc3d
Jesus-fhz <jesusflores9325@gmail.com>
2022-02-12 13:46:26 +1100

    Using hash and array comaparasion benchmark

--- fibonacci.rb
+++ fibonacci.rb
@@ -1,6 +1,6 @@
 #Recursive Fibonacci sequence
 require 'benchmark/ips'
-def fib_loop(n = 8) 
+def fib_loop(n = 30) 
     a = 1
     b = 1
     n.times do 
@@ -11,7 +11,7 @@ def fib_loop(n = 8)
     a
 end
 
- def fib_rec_no_memoization(n = 8) 
+ def fib_rec_no_memoization(n = 30) 
     if n < 2
         return 1
     else
@@ -20,14 +20,23 @@ end
  end
 
 
-@hash_memory = {}
- def fib_rec_with_memoization(n = 8)
-    if @hash_memory.has_key?(n) 
-        @hash_memory[n]
+@hash_memory_object = {}
+ def fib_rec_with_memoization_hash(n = 30)
+    if @hash_memory_object.has_key?(n) 
+        @hash_memory_object[n]
     elsif n < 2
         1
     else
-        @hash_memory[n] = fib_rec_with_memoization(n-1) + fib_rec_with_memoization(n-2)
+        @hash_memory_object[n] = fib_rec_with_memoization_hash(n-1) + fib_rec_with_memoization_hash(n-2)
+    end
+ end
+
+ @hash_memory_array = []
+ def fib_rec_with_memoization_array(n = 30)
+    if n < 2
+        1
+    else
+        @hash_memory_array[n] || @hash_memory_array[n] = fib_rec_with_memoization_array(n-1) + fib_rec_with_memoization_array(n-2)
     end
  end
  
@@ -35,6 +44,7 @@ end
  Benchmark.ips do |x|
     x.report('fib_loop: ')        { fib_loop() }
     x.report('fib_rec_no_memoization: ') { fib_rec_no_memoization() }
-    x.report('fib_rec_with_memoization: ') { fib_rec_with_memoization() }
+    x.report('fib_rec_with_memoization_hash: ') { fib_rec_with_memoization_hash() }
+    x.report('fib_rec_with_memoization_array: ') { fib_rec_with_memoization_array() }
     x.compare!
 end
